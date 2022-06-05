7ea1ac094204f382ddde1939867361d7cd8f6d51
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2022-06-06 00:17:34 +0700

    for deploying

--- frontend/index.js
+++ frontend/index.js
@@ -2,8 +2,7 @@ const BG_COLOR = '#231f20'
 const SNAKE_COLOR = '#c2c2c2'
 const FOOD_COLOR = '#e66916'
 
-// const socket = io("https://arcane-hollows-21167.herokuapp.com/")
-const socket = io("http://localhost:3000")
+const socket = io("https://cryptic-castle-71555.herokuapp.com/")
 socket.on('init',handleInit)
 socket.on('gameState',handleGameState)
 socket.on('gameOver',handleGameOver)
