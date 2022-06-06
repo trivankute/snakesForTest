607984e2a2f745dba2e730536e3cd699486b3316
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2022-06-06 12:47:34 +0700

    for deploying

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,8 @@ const BG_COLOR = '#231f20'
 const SNAKE_COLOR = '#c2c2c2'
 const FOOD_COLOR = '#e66916'
 
-const socket = io("https://cryptic-castle-71555.herokuapp.com/")
+const socket = io("https://snakefortesting.herokuapp.com/")
+// const socket = io("http://localhost:3000")
 socket.on('init',handleInit)
 socket.on('gameState',handleGameState)
 socket.on('gameOver',handleGameOver)
